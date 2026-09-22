780198e3654372032bd6f66d60292c28bca31836
manuk95 <k.nir@hotmail.com>
2026-09-22 23:17:01 +0200

    Automatische Sitemap für bewusst nicht indexierte Website deaktivieren

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import autoLink from './src/markdown/autolink.mjs';
 import dayPlan from './src/markdown/day-plan.mjs';
-export default defineConfig({ site:'https://manuk95.github.io', base:'/reisen', output:'static', markdown:{rehypePlugins:[autoLink,dayPlan]}, integrations:[mdx(),sitemap()], trailingSlash:'always' });
+
+export default defineConfig({
+  site: 'https://manuk95.github.io',
+  base: '/reisen',
+  output: 'static',
+  markdown: { rehypePlugins: [autoLink, dayPlan] },
+  integrations: [mdx()],
+  trailingSlash: 'always',
+});
